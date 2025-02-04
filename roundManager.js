8f1189c9de33409d40366fff409a56ee9354b7ec
AltcoinDaddy <affiakalu@gmail.com>
2025-02-04 21:10:03 +0100

    Fixed scoring and round ending

--- roundManager.js
+++ roundManager.js
@@ -4,22 +4,33 @@ class RoundManager {
         this.questionsInRound = 0;
         this.maxQuestions = 5;
         this.isRoundActive = false;
+        this.scores = new Map();
     }
 
     startNewRound() {
         this.currentRound++;
         this.questionsInRound = 0;
         this.isRoundActive = true;
+        this.scores.clear();
         return this.currentRound;
     }
 
     addQuestion() {
         this.questionsInRound++;
-        if (this.questionsInRound >= this.maxQuestions) {
-            this.isRoundActive = false;
-            return true;
-        }
-        return false;
+        return this.questionsInRound >= this.maxQuestions;
+    }
+
+    addScore(userId) {
+        const currentScore = this.scores.get(userId) || 0;
+        this.scores.set(userId, currentScore + 1);
+        return this.scores.get(userId);
+    }
+
+    getScores() {
+        return Array.from(this.scores.entries()).map(([userId, score]) => ({
+            userId,
+            score
+        })).sort((a, b) => b.score - a.score);
     }
 }
 
